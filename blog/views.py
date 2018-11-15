3906a8cb7e2002072011975b79a098c1df51a548
Urzhumtsev <urzhumtsev.max@gmail.com>
2018-11-15 15:33:42 +0300

    f: Update posts

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
 	ListView, 
 	DetailView, 
-	CreateView
+	CreateView,
+	UpdateView
 )
 # from django.urls import reverse_lazy
 from .models import Post
@@ -24,7 +26,7 @@ class PostDetailView(DetailView):
 	model = Post
 	
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
 	model = Post
 	fields = ['title', 'content']
 	# success_url = reverse_lazy('blog-home') 
@@ -34,9 +36,23 @@ class PostCreateView(CreateView):
 		form.instance.author = self.request.user
 		return super().form_valid(form)
 
-	
-	
 
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+	# success_url = reverse_lazy('blog-home') 
+	# If we want user get back on homepage after new post created
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		return False
+	
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
